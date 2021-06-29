6cd5c8d0c164e40ffcf0cdca1dc5c7db6ef7f70b
GonArr1996 <86024226+GonArr1996@users.noreply.github.com>
2021-06-28 19:41:33 -0500

    Create EOD.R

---
+++ EOD.R
@@ -0,0 +1,11 @@
+
+
+getwd()
+
+setwd("/Users/gonzaloarrangoiz/Desktop/Clases de R WRI")
+
+Vivienda <- read.csv("tvivienda.csv")
+Hogar <- read.csv("thogar.csv")
+PerfilSociodemografico <- read.csv("tsdem.csv")
+Viajes <- read.csv("tviaje.csv")
+Transporte <- read.csv("ttransporte.csv")
