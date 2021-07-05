b82a9a7be9e9cf8b4f1913428e2dfe623aef11c3
GonArr1996 <86024226+GonArr1996@users.noreply.github.com>
2021-07-04 21:40:49 -0500

    Update EOD.R

--- EOD.R
+++ EOD.R
@@ -1,11 +1,33 @@
+# Script análisis bases de datos EOD-2017
 
+## Crear y establacer directorios de trabajo
 
-getwd()
+setwd("~/")
 
-setwd("/Users/gonzaloarrangoiz/Desktop/Clases de R WRI")
+if (!dir.exists("analisis_eod")) {
+      dir.create("analisis_eod")
+}
+setwd("~/analisis_eod")
 
-Vivienda <- read.csv("tvivienda.csv")
-Hogar <- read.csv("thogar.csv")
-PerfilSociodemografico <- read.csv("tsdem.csv")
-Viajes <- read.csv("tviaje.csv")
-Transporte <- read.csv("ttransporte.csv")
+### URL que contiene la descarga del archivo comprimido con las bases de datos:
+url_eod <- "https://www.inegi.org.mx/contenidos/programas/eod/2017/datosabiertos/eod_2017_csv.zip"
+
+### Variable con el nombre del archivo que interesa
+eod_file <- "eod_2017_csv.zip"
+
+### Verificación y descarga de bases de datos 
+if (!file.exists(eod_file)) {
+      download.file(url_eod, eod_file, mode = "wb")
+}
+
+if (!dir.exists("eod_2017_csv")) {
+      unzip(eod_file)
+}
+
+### Cargar CSVs de bases de datos en sesion de R.
+library(readr)
+Hogar <- read_csv("thogar_eod2017/conjunto_de_datos/thogar.csv")
+Vivienda <- read_csv("tvivienda_eod2017/conjunto_de_datos/tvivienda.csv")
+PerfilSociodemografico <- read_csv("tsdem_eod2017/conjunto_de_datos/tsdem.csv")
+Viajes <- read_csv("tviaje_eod2017/conjunto_de_datos/tviaje.csv")
+Transporte <- read_csv("ttransporte_eod2017/conjunto_de_datos/ttransporte.csv")
